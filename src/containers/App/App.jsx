673a0d1a9b4f165d48c040fb1580a226ec55ac8d
Asirg <vojt.tieg295i@gmail.com>
2022-10-11 17:40:53 +0300

    init

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <h1>Hellow world</h1>
+  )
+}
+
+App.propTypes = {
+  prop: PropTypes.string
+}
+
+export default App;
